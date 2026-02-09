23aba5b3e2294398a2d85dbf00b87cf8972e72ab
Lavish Agarwal <agrwl.lavish@gmail.com>
2026-02-09 11:47:51 +0530

    Refactor Header component for consistent formatting

--- FrontEnd/ReactDemo/vite-project/src/components/Header.jsx
+++ FrontEnd/ReactDemo/vite-project/src/components/Header.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
     <div>
-        <nav style={{ padding: '10px', backgroundColor: '#4A90E2', color: '#fff' }}>
-            <h1>Fashion Store</h1>
-            Home | Products | About | Contact
-
-        </nav>
+      <nav
+        style={{ padding: "10px", backgroundColor: "#4A90E2", color: "#fff" }}
+      >
+        <h1>Fashion Store</h1>
+        Home | Products | About | Contact
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
